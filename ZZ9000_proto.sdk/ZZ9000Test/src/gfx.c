1682eb2f7590c77e5b3efbe6c79b66c45291e4b0
mntmn <lukas@mntmn.com>
2019-03-29 00:02:38 +0100

    WIP handle overlapping rect copies

--- ZZ9000_proto.sdk/ZZ9000Test/src/gfx.c
+++ ZZ9000_proto.sdk/ZZ9000Test/src/gfx.c
@@ -52,28 +52,76 @@ void fill_rect16(uint16_t rect_x1, uint16_t rect_y1, uint16_t rect_x2, uint16_t 
 }
 
 void copy_rect32(uint16_t rect_x1, uint16_t rect_y1, uint16_t rect_x2, uint16_t rect_y2, uint16_t rect_sx, uint16_t rect_sy) {
-	for (uint16_t sy=rect_sy, dy=rect_y1; dy<=rect_y2; sy++, dy++) {
+	int8_t ystep=1, xstep=1;
+	uint16_t tmp;
+	if (rect_sy < rect_y1) {
+		uint16_t h = rect_y2-rect_y1;
+		ystep=-1;
+		tmp=rect_y2; rect_y2=rect_y1; rect_y1=tmp;
+		rect_sy+=h;
+	}
+	if (rect_sx < rect_x1) {
+		uint16_t w = rect_x2-rect_x1;
+		xstep=-1;
+		tmp=rect_x2; rect_x2=rect_x1; rect_x1=tmp;
+		rect_sx+=w;
+	}
+	rect_y2+=ystep;
+	rect_x2+=xstep;
+	for (uint16_t sy=rect_sy, dy=rect_y1; dy!=rect_y2; sy+=ystep, dy+=ystep) {
 		uint32_t* dp=(uint32_t*)(fb+dy*fb_pitch);
 		uint32_t* sp=(uint32_t*)(fb+sy*fb_pitch);
-		for (uint16_t sx=rect_sx, dx=rect_x1; dx<=rect_x2; sx++, dx++) {
+		for (uint16_t sx=rect_sx, dx=rect_x1; dx!=rect_x2; sx+=xstep, dx+=xstep) {
 			dp[dx]=sp[sx];
 		}
 	}
 }
 void copy_rect16(uint16_t rect_x1, uint16_t rect_y1, uint16_t rect_x2, uint16_t rect_y2, uint16_t rect_sx, uint16_t rect_sy) {
-	for (uint16_t sy=rect_sy, dy=rect_y1; dy<=rect_y2; sy++, dy++) {
+	int8_t ystep=1, xstep=1;
+	uint16_t tmp;
+	if (rect_sy < rect_y1) {
+		uint16_t h = rect_y2-rect_y1;
+		ystep=-1;
+		tmp=rect_y2; rect_y2=rect_y1; rect_y1=tmp;
+		rect_sy+=h;
+	}
+	if (rect_sx < rect_x1) {
+		uint16_t w = rect_x2-rect_x1;
+		xstep=-1;
+		tmp=rect_x2; rect_x2=rect_x1; rect_x1=tmp;
+		rect_sx+=w;
+	}
+	rect_y2+=ystep;
+	rect_x2+=xstep;
+	for (uint16_t sy=rect_sy, dy=rect_y1; dy!=rect_y2; sy+=ystep, dy+=ystep) {
 		uint16_t* dp=(uint16_t*)(fb+dy*fb_pitch);
 		uint16_t* sp=(uint16_t*)(fb+sy*fb_pitch);
-		for (uint16_t sx=rect_sx, dx=rect_x1; dx<=rect_x2; sx++, dx++) {
+		for (uint16_t sx=rect_sx, dx=rect_x1; dx!=rect_x2; sx+=xstep, dx+=xstep) {
 			dp[dx]=sp[sx];
 		}
 	}
 }
 void copy_rect8(uint16_t rect_x1, uint16_t rect_y1, uint16_t rect_x2, uint16_t rect_y2, uint16_t rect_sx, uint16_t rect_sy) {
-	for (uint16_t sy=rect_sy, dy=rect_y1; dy<=rect_y2; sy++, dy++) {
+	int8_t ystep=1, xstep=1;
+	uint16_t tmp;
+	if (rect_sy < rect_y1) {
+		uint16_t h = rect_y2-rect_y1;
+		ystep=-1;
+		tmp=rect_y2; rect_y2=rect_y1; rect_y1=tmp;
+		rect_sy+=h;
+	}
+	if (rect_sx < rect_x1) {
+		uint16_t w = rect_x2-rect_x1;
+		xstep=-1;
+		tmp=rect_x2; rect_x2=rect_x1; rect_x1=tmp;
+		rect_sx+=w;
+	}
+	rect_y2+=ystep;
+	rect_x2+=xstep;
+	for (uint16_t sy=rect_sy, dy=rect_y1; dy!=rect_y2; sy+=ystep, dy+=ystep) {
 		uint8_t* dp=(uint8_t*)(fb+dy*fb_pitch);
 		uint8_t* sp=(uint8_t*)(fb+sy*fb_pitch);
-		for (uint16_t sx=rect_sx, dx=rect_x1; dx<=rect_x2; sx++, dx++) {
+		for (uint16_t sx=rect_sx, dx=rect_x1; dx!=rect_x2; sx+=xstep, dx+=xstep) {
 			dp[dx]=sp[sx];
 		}
 	}
