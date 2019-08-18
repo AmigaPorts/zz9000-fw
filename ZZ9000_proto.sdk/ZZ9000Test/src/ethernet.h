5b553ba4566913e6a4018bd084fc54ffe4858723
mntmn <lukas@mntmn.com>
2019-08-18 20:31:29 +0200

    add missing ethernet.h

---
+++ ZZ9000_proto.sdk/ZZ9000Test/src/ethernet.h
@@ -0,0 +1,38 @@
+/*
+ * MNT ZZ9000 Amiga Graphics and Coprocessor Card Operating System (ZZ9000OS)
+ *
+ * Copyright (C) 2019, Lukas F. Hartmann <lukas@mntre.com>
+ *                     MNT Research GmbH, Berlin
+ *                     https://mntre.com
+ *
+ * More Info: https://mntre.com/zz9000
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * GNU General Public License v3.0 or later
+ *
+ * https://spdx.org/licenses/GPL-3.0-or-later.html
+ *
+*/
+
+#ifndef ETHERNET_H_
+#define ETHERNET_H_
+
+int init_ethernet();
+u16 ethernet_send_frame(u16 frame_size);
+void ethernet_receive_frame();
+u32 get_frames_received();
+uint8_t* ethernet_get_mac_address_ptr();
+void ethernet_update_mac_address();
+
+// FIXME allocate this memory properly
+
+#define RX_BD_LIST_START_ADDRESS	0x02110000
+#define TX_BD_LIST_START_ADDRESS	0x02180000
+#define RX_FRAME_ADDRESS 0x02100000
+#define TX_FRAME_ADDRESS 0x02108000
+#define RX_FRAME_PAD (-4)
+
+#define RXBD_CNT       1	/* Number of RxBDs to use */
+#define TXBD_CNT       2	/* Number of TxBDs to use */
+
+#endif
